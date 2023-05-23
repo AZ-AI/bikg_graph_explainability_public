4bd9acfd8193a6a7c8a07e39d0765bb1ab59655f
andres2631996 <47141286+andres2631996@users.noreply.github.com>
2023-05-23 12:34:14 +0200

    Add files via upload

---
+++ dev_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ml gcccuda
+pip install -r src/requirements.txt
+pip install bikg -i https://artifactory.astrazeneca.net/api/pypi/pypi-virtual/simple
+pip install torch==1.10.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+
+pip install torch-cluster==1.6.0 
+pip install torch-scatter==2.0.9
+pip install torch-sparse==0.6.12
+pip install torch-geometric==2.0.4
+conda install -c conda-forge cupy cudatoolkit=10.2
+pip install -e .
